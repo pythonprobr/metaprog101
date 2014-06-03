dc6bce54c13ba1ae9f242d3db91310be62c245da
Luciano Ramalho <luciano@ramalho.org>
2014-06-03 12:15:31 -0300

    first solution

--- datamodel/exercises/monolist.py
+++ datamodel/exercises/monolist.py
@@ -19,13 +19,58 @@ Operações básicas:
 
 Verificação de tipos:
 
-    >>> l.append('x')
+    >>> l.append('x')  # doctest: -SKIP
     Traceback (most recent call last):
       ...
-    TypeError: 'x' is not the same type as 3 (int)
-    >>> l.extend([10, [11]])
+    TypeError: 'x' (str) is not the same type as 3 (int)
+    >>> l.extend([10, [11]])  # doctest: -SKIP
     Traceback (most recent call last):
       ...
-    TypeError: '[11]' is not the same type as 3 (int)
+    TypeError: [11] (list) is not the same type as 3 (int)
+    >>> len(l)
+    5
+
 
 """
+
+import UserList
+
+
+class MonoList(UserList.UserList):
+
+    def append(self, item):
+        if self.data:
+            self._check_type(item)
+        self.data.append(item)
+
+    def __repr__(self):
+        return '%s(%r)' % (type(self).__name__, self.data)
+
+
+    def _check_type(self, item):
+        first = self.data[0]
+        if type(first) is not type(item):
+            msg = '%r (%s) is not the same type as %r (%s)'
+            raise TypeError(msg % (item, type(item).__name__,
+                                   first, type(first).__name__))
+
+
+    def extend(self, iterable):
+        new_items = []
+        for item in iterable:
+            self._check_type(item)
+            new_items.append(item)
+        self.data.extend(new_items)
+
+
+
+
+
+
+
+
+
+
+
+
+

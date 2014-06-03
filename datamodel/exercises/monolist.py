67cf01c7e8a3a37d158d1b36cbdc260609218c2d
Luciano Ramalho <luciano@ramalho.org>
2014-06-03 11:51:57 -0300

    reordering tests for mono-typed list

--- datamodel/exercises/monolist.py
+++ datamodel/exercises/monolist.py
@@ -1,23 +1,31 @@
+# coding: utf-8
+
 """
 
->>> l = MonoList()
->>> l.append(3)
->>> len(l)
-1
->>> l.append('x')
-Traceback (most recent call last):
-  ...
-TypeError: 'x' is not the same type as 3 (int)
->>> l.extend(range(4))
->>> len(l)
-5
->>> l.extend([10, [11]])
-Traceback (most recent call last):
-  ...
-TypeError: '[11]' is not the same type as 3 (int)
->>> len(l)
-5
->>> l
-MonoList([3, 0, 1, 2, 3])
+Operações básicas:
+
+    >>> l = MonoList()
+    >>> l.append(3)
+    >>> len(l)
+    1
+    >>> l
+    MonoList([3])
+    >>> l.extend(range(4))
+    >>> len(l)
+    5
+    >>> l
+    MonoList([3, 0, 1, 2, 3])
+
+
+Verificação de tipos:
+
+    >>> l.append('x')
+    Traceback (most recent call last):
+      ...
+    TypeError: 'x' is not the same type as 3 (int)
+    >>> l.extend([10, [11]])
+    Traceback (most recent call last):
+      ...
+    TypeError: '[11]' is not the same type as 3 (int)
 
 """

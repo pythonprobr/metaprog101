f78c86d6458c45474b0b20879f97c7d38ba28cc4
Luciano Ramalho <luciano@ramalho.org>
2014-06-03 11:45:59 -0300

    further tests for mono-typed list

--- datamodel/exercises/monolist.py
+++ datamodel/exercises/monolist.py
@@ -7,6 +7,14 @@
 >>> l.append('x')
 Traceback (most recent call last):
   ...
-TypeError: 'x' is not the same type as 3
+TypeError: 'x' is not the same type as 3 (int)
+>>> l.extend(range(4))
+>>> len(l)
+5
+>>> l.extend([10, [11]])
+Traceback (most recent call last):
+  ...
+TypeError: '[11]' is not the same type as 3 (int)
+
 
 """
